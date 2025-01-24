1b99a968e02a82f1b3177ec7af5d8b6fcb7b7a35
david100awesome <32517214+david100awesome@users.noreply.github.com>
2025-01-24 16:55:53 -0600

    startingstrong

---
+++ mainpage/Assests/JS/index.js
@@ -0,0 +1,26 @@
+var ds = [
+ 
+];
+
+function splashText() {
+ if(ds[0] == "209.235.191.130")
+ {
+  document.querySelector(".Index-SplashText").innerHTML =
+    "You Guys are Awesome, Keep Up the Good Work";
+ } else
+ {
+
+ }
+}
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  splashText();
+
+ fetch("https://api.ipify.org?format=json")
+    .then((response) => response.json())
+    .then((data) => {
+      ipAddress = data.ip;
+      ds.push(`${ipAddress}`);
+      splashText();
+    });
+});
